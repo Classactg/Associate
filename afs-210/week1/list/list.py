1ff0eb1ea36e1640641e014a4071a6f31f057287
Classactg <benjamin.simpson@bryanuniversity.edu>
2021-05-16 11:41:42 -0700

    Week1

---
+++ afs-210/week1/list/list.py
@@ -0,0 +1,8 @@
+fixedOnNothing = []
+tripleOggiList = [25, 45, 36, 47, 69, 48, 68, 78, 14, 36]
+tripleOggiList.sort()
+for num in tripleOggiList:
+    # print(num)
+    fixedOnNothing.append(num)
+
+print(fixedOnNothing)   
