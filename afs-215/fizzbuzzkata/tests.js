9aa2a85a3666a3226ab0aab7fa60208a09352450
Classactg <benjamin.simpson@bryanuniversity.edu>
2021-08-03 14:11:16 -0700

    Fizzbuzz JS pt.1

---
+++ afs-215/fizzbuzzkata/tests.js
@@ -0,0 +1,12 @@
+// const assert = require('chai').assert;
+// const fizzbuzz = require ("./Fizzbuzzkata")
+
+// var a = "1"
+
+// describe('fizzbuzz test', function(){
+
+//     it('check the dish has valid name.', function(){
+//         assert.equal(a, '1','eureka');
+//     })
+// }
+// )
