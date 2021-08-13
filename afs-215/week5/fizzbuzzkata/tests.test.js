82c09a9bcd9ba02cb09dcc252f0311e1047a0606
Classactg <benjamin.simpson@bryanuniversity.edu>
2021-08-12 18:20:09 -0700

    FizzBuzz

--- afs-215/week5/fizzbuzzkata/tests.test.js
+++ afs-215/week5/fizzbuzzkata/tests.test.js
@@ -26,5 +26,40 @@ const actual =  fizzbuzz(2);
 expect(actual).to.deep.equal(expected)
 
     })
+    it('Get "Fizz" when I pass in 3.', function(){
+        const expected = "Fizz";
+const actual =  fizzbuzz(3);
+
+expect(actual).to.deep.equal(expected)
+
+    })
+    it('Get "Buzz" when I pass in 5.', function(){
+        const expected = "Buzz";
+const actual =  fizzbuzz(5);
+
+expect(actual).to.deep.equal(expected)
+
+    })
+    it('Get "Fizz" when I pass in 6.', function(){
+        const expected = "Fizz";
+const actual =  fizzbuzz(6);
+
+expect(actual).to.deep.equal(expected)
+
+    })
+    it('Get "Buzz" when I pass in 10.', function(){
+        const expected = "Buzz";
+const actual =  fizzbuzz(10);
+
+expect(actual).to.deep.equal(expected)
+
+    })
+    it('Get "FizzBuzz" when I pass in 15.', function(){
+        const expected = "FizzBuzz";
+const actual =  fizzbuzz(15);
+
+expect(actual).to.deep.equal(expected)
+
+    })
 }
 )
