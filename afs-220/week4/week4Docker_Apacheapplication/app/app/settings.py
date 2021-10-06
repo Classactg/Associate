63d88f058c73d1cf22091d4619672fef3bf66bde
Classactg <benjamin.simpson@bryanuniversity.edu>
2021-10-06 14:06:49 -0700

    Continuing Pages

--- afs-220/week4/week4Docker_Apacheapplication/app/app/settings.py
+++ afs-220/week4/week4Docker_Apacheapplication/app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -55,10 +55,26 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'app.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+print (BASE_DIR)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'hello_world/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- afs-220/week4/week4Docker_Apacheapplication/app/hello_world/views.py
+++ afs-220/week4/week4Docker_Apacheapplication/app/hello_world/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
 
+class IndexView(generic.ListView):
+    # """Placeholder index view"""
+    template_name = '/index.html'
+    def get_queryset(self):
+        pass
+    # return render(request, 'index.html')
+class LocationsView(generic.ListView):
+    # """Placeholder index view"""
+    template_name = '/locations.html'
+class GallaryView(generic.ListView):
+    # """Placeholder index view"""
+    template_name = '/gallary.html'
+class AboutView(generic.ListView):
+    # """Placeholder index view"""
+    template_name = '/about.html'
+class ServicesView(generic.ListView):
+    # """Placeholder index view"""
+    template_name = '/services.html'
+class ContactsView(generic.ListView):
+    # """Placeholder index view"""
+    template_name = '/contacts.html' 
 
-def index(request):
-    """Placeholder index view"""
-    return render(request, 'index.html')
